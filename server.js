7f65602c34ca583b5e4b4f040a411ad43402db27
Bhuvan P <vishal.p.patil.p5@gmail.com>
2023-04-08 13:04:09 +0100

    portport

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 // serving the index.html file 
 
-const server = app.listen(process.env.server||5000);
+const server = app.listen(process.env.PORT||5000);
 const portNumber = server.address().port;
 console.log(`port: ${portNumber}`);
 // can see the port number in terminal - you can dictate the port number
